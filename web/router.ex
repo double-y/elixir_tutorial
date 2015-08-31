cb7a601d71df8b96e6cc1dba32e57baf600622e1
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-08-31 23:32:40 +0900

    make terms controller

--- web/router.ex
+++ web/router.ex
@@ -18,6 +18,7 @@ defmodule PhoenixTutorial.Router do
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
+    resources "/terms", TermController
   end
 
   # Other scopes may use custom stacks.
