f77557b84d8a91c249ad80e5b0df8488693cdd97
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-09-07 23:07:25 +0900

    add 404 page

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -2,7 +2,7 @@ defmodule PhoenixTutorial.ErrorView do
   use PhoenixTutorial.Web, :view
 
   def render("404.html", _assigns) do
-    "Page not found"
+    render("not_found.html", %{})
   end
 
   def render("500.html", _assigns) do
