368f8884bdd52b9f28525456f532b2892796c503
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-09-22 10:58:50 -0400

    change test cofigure to exclude error_view_case

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,4 @@ Mix.Task.run "ecto.create", ["--quiet"]
 Mix.Task.run "ecto.migrate", ["--quiet"]
 Ecto.Adapters.SQL.begin_test_transaction(PhoenixTutorial.Repo)
 
+ExUnit.configure(exclude: [:error_view_case]) 
