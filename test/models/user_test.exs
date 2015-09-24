4a3e71f5e4151dc70d4037c8abd05cc5e4ff9e81
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-09-24 09:54:11 -0700

    add number_of_pets is not required in User

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -6,6 +6,11 @@ defmodule PhoenixTutorial.UserTest do
   @valid_attrs %{bio: "my life", email: "pat@example.com", name: "Pat Example", number_of_pets: 4}
   @invalid_attrs %{}
 
+  test "number_of_pets is not required" do
+    changeset = User.changeset(%User{}, Map.delete(@valid_attrs, :number_of_pets))
+    assert changeset.valid?
+  end
+
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
     assert changeset.valid?
